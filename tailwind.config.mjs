0e6654d114495f45c03af9b2a7d419966a72b22d
hafizfarhad <mrhafizfarhad@gmail.com>
2025-07-09 13:11:35 +0500

    fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,8 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Verdana', 'Geneva', 'sans-serif'],
-      },
+    fontFamily: {
+      'sans': ['Verdana', 'Geneva', 'sans-serif'],
     },
   },
   plugins: [],
